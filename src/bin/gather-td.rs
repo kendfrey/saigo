fb15834f8d3a20f5f2f84be6b3d3472db89f3656
Kendall Frey <kendfrey@gmail.com>
2024-11-04 22:24:48 -0500

    gather training data

--- src/bin/gather-td.rs
+++ src/bin/gather-td.rs
@@ -1,14 +1,58 @@
-use std::{thread::sleep, time::Duration};
+use std::{
+    fs::{self, File},
+    path::PathBuf,
+    time::{Duration, Instant},
+};
 
-use saigo::ControlMessage;
+use clap::Parser;
+use image::{buffer::ConvertBuffer, RgbImage};
+use saigo::{deserialize_image, ControlMessage};
 use tungstenite::{connect, Message};
 
-fn main() {
-    let (mut socket, _) = connect("ws://localhost:5410/ws/control").unwrap();
-
-    loop {
-        let json = serde_json::to_string(&ControlMessage::NewTrainingPattern).unwrap();
-        _ = socket.send(Message::Text(json));
-        sleep(Duration::from_secs(2));
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+    if args.out.try_exists()? {
+        println!(
+            "Directory {} already exists and will not be overwritten.",
+            args.out.display()
+        );
+        return Ok(());
     }
+
+    let (mut control_socket, _) = connect("ws://localhost:5410/ws/control").unwrap();
+    let (mut camera_socket, _) = connect("ws://localhost:5410/ws/board-camera").unwrap();
+    let new_training_pattern =
+        Message::Text(serde_json::to_string(&ControlMessage::NewTrainingPattern).unwrap());
+    _ = control_socket.send(new_training_pattern.clone());
+
+    fs::create_dir_all(&args.out)?;
+
+    reqwest::blocking::Client::new()
+        .post("http://localhost:5410/api/config/camera/reference?take=false")
+        .send()?
+        .error_for_status()?
+        .copy_to(&mut File::create_new(args.out.join("reference.png"))?)?;
+
+    let interval = Duration::from_secs(5);
+    let mut next_capture = Instant::now() + interval;
+    let mut num = 0;
+    loop {
+        if let Message::Binary(data) = camera_socket.read()? {
+            if Instant::now() >= next_capture {
+                let image: RgbImage = deserialize_image(data).convert();
+                image.save(args.out.join(format!("{}.png", num)))?;
+                num += 1;
+
+                next_capture += interval;
+                _ = control_socket.send(new_training_pattern.clone());
+            }
+        }
+    }
+}
+
+/// Gathers training data for the image recognition neural network.
+#[derive(Parser)]
+struct Args {
+    /// The directory to save the training data to.
+    out: PathBuf,
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use image::RgbaImage;
 use serde::{Deserialize, Serialize};
 
 /// The width of a stone in pixels on the normalized image of the board.
@@ -8,4 +9,15 @@ pub const STONE_SIZE: u32 = 16;
 #[serde(tag = "type", rename_all = "snake_case")]
 pub enum ControlMessage {
     NewTrainingPattern,
+}
+
+/// Deserializes an image from a binary message.
+/// The first 4 bytes of the message are the width of the image.
+/// The next 4 bytes are the height of the image.
+/// The rest of the message is the image data, in 32-bit RGBA format.
+pub fn deserialize_image(message: Vec<u8>) -> RgbaImage {
+    let width = u32::from_be_bytes(message[0..4].try_into().unwrap());
+    let height = u32::from_be_bytes(message[4..8].try_into().unwrap());
+    let data = message[8..].to_vec();
+    RgbaImage::from_raw(width, height, data).unwrap()
 }
