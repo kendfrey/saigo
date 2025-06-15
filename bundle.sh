c675052a51e8be08038022a2418d0ec7f6d694f7
Kendall Frey <kendfrey@gmail.com>
2025-06-15 15:38:58 -0400

    update bundle script

--- bundle.sh
+++ bundle.sh
@@ -1,9 +1,10 @@
 shopt -s extglob
-mkdir -p dist/x86_64-pc-windows-msvc dist/x86_64-unknown-linux-gnu
-cp target/x86_64-pc-windows-msvc/release/*.exe model.safetensors model.txt dist/x86_64-pc-windows-msvc
-cp -r html dist/x86_64-pc-windows-msvc
-cp target/x86_64-unknown-linux-gnu/release/!(*.*) model.safetensors model.txt dist/x86_64-unknown-linux-gnu
-cp -r html dist/x86_64-unknown-linux-gnu
+export VERSION=$(grep -m 1 "^version = " < Cargo.toml | grep -oP [\\d.]+)
+mkdir -p dist/saigo-v$VERSION-x86_64-pc-windows-msvc dist/saigo-v$VERSION-x86_64-unknown-linux-gnu
+cp target/x86_64-pc-windows-msvc/release/*.exe model.safetensors model.txt dist/saigo-v$VERSION-x86_64-pc-windows-msvc
+cp -r html dist/saigo-v$VERSION-x86_64-pc-windows-msvc
+cp target/x86_64-unknown-linux-gnu/release/!(*.*) model.safetensors model.txt dist/saigo-v$VERSION-x86_64-unknown-linux-gnu
+cp -r html dist/saigo-v$VERSION-x86_64-unknown-linux-gnu
 cd dist
-zip -r x86_64-pc-windows-msvc.zip x86_64-pc-windows-msvc
-zip -r x86_64-unknown-linux-gnu.zip x86_64-unknown-linux-gnu
+zip -r saigo-v$VERSION-x86_64-pc-windows-msvc.zip saigo-v$VERSION-x86_64-pc-windows-msvc
+zip -r saigo-v$VERSION-x86_64-unknown-linux-gnu.zip saigo-v$VERSION-x86_64-unknown-linux-gnu
